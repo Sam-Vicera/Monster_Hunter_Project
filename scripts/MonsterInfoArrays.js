91872af1dbed577c4a8d7e28b1775b9158e12a78
Sam-Vicera <shernandezvicera@gmail.com>
2023-11-10 20:43:38 -0700

    array of info started

---
+++ scripts/MonsterInfoArrays.js
@@ -0,0 +1,320 @@
+
+let monsters = [
+    {
+        "beastName": "Great Izuchi", 
+        "smallCrownSize": 967.58,
+        "largeCrownSize": 1322.37,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+      
+    {
+        "beastName": "Great Baggi", 
+        "smallCrownSize": 915.88,
+        "largeCrownSize":1272.06,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Kulu-Ya-Ku", 
+        "smallCrownSize": 811.11,
+        "largeCrownSize":1108.52,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Great Wroggi", 
+        "smallCrownSize": 842.38,
+        "largeCrownSize":1151.25,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Arzuros", 
+        "smallCrownSize": 491.12,
+        "largeCrownSize":692.4,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Lagombi", 
+        "smallCrownSize": 472.59,
+        "largeCrownSize":645.88,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Volvidon", 
+        "smallCrownSize": 604.81,
+        "largeCrownSize":859.11,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Aknosom", 
+        "smallCrownSize": 763.72,
+        "largeCrownSize":1060.72,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Royal Ludroth", 
+        "smallCrownSize": 1376.33,
+        "largeCrownSize":1880.98,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Barroth", 
+        "smallCrownSize": 1244.76,
+        "largeCrownSize":1728.83,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Khezu", 
+        "smallCrownSize": 793.15,
+        "largeCrownSize":1004.65,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Tetranadon", 
+        "smallCrownSize": 868.49,
+        "largeCrownSize":1186.93,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Bishaten", 
+        "smallCrownSize": 832.31,
+        "largeCrownSize":1054.26,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Pukei-Pukei", 
+        "smallCrownSize": 981.18,
+        "largeCrownSize":1356.1,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Jyuratodus", 
+        "smallCrownSize": 1357.83,
+        "largeCrownSize":1870.8,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Basarios", 
+        "smallCrownSize": 1271.7,
+        "largeCrownSize":1766.26,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Somnacanth", 
+        "smallCrownSize": 1390.49,
+        "largeCrownSize":1900.35,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Rathian", 
+        "smallCrownSize": 1578.93,
+        "largeCrownSize":2157.87,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Barioth", 
+        "smallCrownSize": 1881.89,
+        "largeCrownSize":2571.93,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Tobi-Kadachi", 
+        "smallCrownSize": 1170.46,
+        "largeCrownSize":1612.64,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+  
+    {
+        "beastName": "Magnamalo", 
+        "smallCrownSize": 1638.8,
+        "largeCrownSize":2239.69,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Anjanath", 
+        "smallCrownSize": 1481.81,
+        "largeCrownSize":2025.14,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Nargacuga", 
+        "smallCrownSize": 1722.72,
+        "largeCrownSize":2354.39,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Mizutsune", 
+        "smallCrownSize": 1648.88,
+        "largeCrownSize":2253.48,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Goss Harag", 
+        "smallCrownSize": 657.87,
+        "largeCrownSize":899.9,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Rathalos", 
+        "smallCrownSize": 1533.79,
+        "largeCrownSize":2096.19,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Almudron", 
+        "smallCrownSize": 2425.4,
+        "largeCrownSize":3314.23,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Zinogre", 
+        "smallCrownSize": 1386.59,
+        "largeCrownSize":1895.1,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Tigrex", 
+        "smallCrownSize": 1747.7,
+        "largeCrownSize":2388.52,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Diablos", 
+        "smallCrownSize": 1886.62,
+        "largeCrownSize":2599.35,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Rakna-Kadaki", 
+        "smallCrownSize": 771.23,
+        "largeCrownSize":1054.2,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Kushala Daora", 
+        "smallCrownSize": 1855.73,
+        "largeCrownSize":2353.14,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Chameleos", 
+        "smallCrownSize": 2136.6,
+        "largeCrownSize":2766.34,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Teostra", 
+        "smallCrownSize": 1736.44,
+        "largeCrownSize":2219.78,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Rajang", 
+        "smallCrownSize": 820.53,
+        "largeCrownSize":1048.92,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Bazelgeuse", 
+        "smallCrownSize": 1735.54,
+        "largeCrownSize":2371.9,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Apex Arzuros", 
+        "smallCrownSize": 681.88,
+        "largeCrownSize":745.81,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Apex Rathian", 
+        "smallCrownSize": 1936.82,
+        "largeCrownSize":2299.98,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+    {
+        "beastName": "Apex Mizu", 
+        "smallCrownSize": 2110.57,
+        "largeCrownSize":2506.31,
+        "pictureUrl": "",
+        "description": "", 
+        "videoUrl": ""
+    },
+      
+    
+   
+]
